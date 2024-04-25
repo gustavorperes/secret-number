a79d8840f62affd94a2b025260b326d85ffd9706
Gustavo Peres <gustavorperes@gmail.com>
2024-04-24 22:21:51 -0300

    Projeto inicial

---
+++ app.js
@@ -0,0 +1,68 @@
+function sorteia(){
+    return Math.round(Math.random()*10)
+}
+
+function sorteiaNumero(quantidade){
+    var sorteados = [];
+    var numero = 1;
+
+    while(numero <= quantidade){
+        var numeroAleatorio = sorteia();
+        
+        if(numeroAleatorio !== 0){
+            var achou = false;
+
+            for(var sort=0 ; sort < sorteados.length ; sort++){
+                if(sorteados[sort] == numeroAleatorio){
+                    achou = true;
+                    break;
+                }
+            }
+
+            if(achou == false){
+                sorteados.push(numeroAleatorio);
+                numero++;
+            }
+        }
+    }
+    
+    return sorteados;
+}
+
+var input = document.querySelector("input");
+var x = sorteiaNumero(3);
+
+function verifica(){          
+    var achou = false;
+    var tentativas = 2;
+    input.focus();
+
+    for(var i=0; i < x.length; i++){
+        if (input.value < x[i]){
+            alert("O numero secreto é maior");
+            var achou = true;
+            tentativas--;
+            console.log(tentativas);
+            break;
+        }
+
+        if (input.value > x[i]){
+            alert("O numero secreto é menor");
+            var achou = true;
+            tentativas--;
+            console.log(tentativas);
+            break;
+        }
+
+        if (input.value == x[i]){
+            alert("acertou");
+            var achou = true;
+            break;
+        }
+    }
+    if(tentativas == 0){
+        alert("O jogo acabou!");
+    }
+    input.value = "";
+    input.focus();
+}
